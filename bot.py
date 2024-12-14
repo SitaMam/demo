4c8106a4bf1644e03c05eb3d7fc6aaf3ab27e0ac
SUBHAJIT MONDAL <123165865+SitaMam@users.noreply.github.com>
2024-12-14 11:42:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="TechVJ"),
+            plugins=dict(root="SubhoBhai"),
             workers=50,
             sleep_threshold=10
         )
